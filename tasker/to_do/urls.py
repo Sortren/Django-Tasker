f6155e98773d79049a5f664d83a8e9355f59b1f5
Maksymilian Biruk <mexberuk@gmail.com>
2021-06-04 00:35:22 +0200

    add to_do home page

--- tasker/to_do/urls.py
+++ tasker/to_do/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test/', views.test, name='to_do-test'),
+    path('', views.home, name='to_do-home'),
     path('login/', views.login, name='to_do-login'),
     path('register/', views.register, name='to_do-register')
 ]
--- tasker/to_do/views.py
+++ tasker/to_do/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def test(request):
-    return HttpResponse('<h1>test</h1>')
+def home(request):
+    return render(request, 'to_do/home.html')
+
 
 def login(request):
     return render(request, 'to_do/login.html')
+
 
 def register(request):
     return render(request, 'to_do/register.html')
