786d5b16a38667c9653b59a300d2da74cf6a5fd6
Maksymilian Biruk <mexberuk@gmail.com>
2021-06-14 16:13:48 +0200

    change textarea widget to specific rows/columns

--- tasker/to_do/forms.py
+++ tasker/to_do/forms.py
@@ -5,4 +5,10 @@ from django import forms
 class AddTaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'content', 'author']
+        fields = ['title', 'content', 'author', 'finished']
+        widgets = {
+            'content': forms.Textarea(attrs={
+                'rows': 3,
+                'cols': 40
+            })
+        }
--- tasker/to_do/models.py
+++ tasker/to_do/models.py
@@ -7,6 +7,7 @@ class Task(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
+    finished = models.BooleanField(default=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
