358d9b6214874399ec6be4b6123c90bb0f8f99da
Maksymilian Biruk <mexberuk@gmail.com>
2021-06-19 14:19:50 +0200

    add 3 fields to model profile

--- tasker/to_do/views.py
+++ tasker/to_do/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 from .models import Task
 from users.models import Profile
 from .forms import AddTaskForm, OrderTasks
--- tasker/users/models.py
+++ tasker/users/models.py
@@ -1,10 +1,15 @@
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     total_tasks_finished = models.IntegerField(default=0)
+    finished_before_deadline = models.IntegerField(default=0)
+    finished_after_deadline = models.IntegerField(default=0)
+    rating = models.IntegerField(default=0, validators=[
+                                 MinValueValidator(0), MaxValueValidator(100)])
 
     def __str__(self):
         return self.user.username
